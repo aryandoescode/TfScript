332289d5acb0d1f61f4aa164b1b690c58222acfa
Aryan Singh <58566951+aryandoescode@users.noreply.github.com>
2023-01-03 16:09:11 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,43 +1,43 @@
 terraform {
-    required_providers {
-        azurerm = {
-            version = "3.37.0"
-            source ="hashicorp/azurerm"
-        }
+  required_providers {
+    azurerm = {
+      version = "3.37.0"
+      source  = "hashicorp/azurerm"
     }
+  }
 }
 
 provider "azurerm" {
-    features {
+  features {
 
-    }
+  }
 }
 
 resource "azurerm_resource_group" "demo" {
-  name = "mytf-rg"
+  name     = "mytf-rg"
   location = "centralindia"
   tags = {
     "terraform" = "terraform"
-  } 
+  }
 }
-resource "azurerm_virtual_network" "myvn"{
-    name = "network"
-    resource_group_name = azurerm_resource_group.demo.name
-    location = azurerm_resource_group.demo.location
-    address_space = ["10.123.0.0/16"]
+resource "azurerm_virtual_network" "myvn" {
+  name                = "network"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  address_space       = ["10.123.0.0/16"]
 }
 resource "azurerm_subnet" "mysubnet" {
-  name = "subnet"
-  resource_group_name = azurerm_resource_group.demo.name
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.myvn.name
-  address_prefixes = ["10.123.1.0/24"]
+  address_prefixes     = ["10.123.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "mynsg" {
-  name = "mynsg"
-  location = azurerm_resource_group.demo.location
+  name                = "mynsg"
+  location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
-  
+
   tags = {
     "terraform" = "terraform"
   }
@@ -54,10 +54,10 @@ resource "azurerm_network_security_group" "mynsg" {
   }
 }
 resource "azurerm_public_ip" "myip" {
-  name = "myip"
+  name                = "myip"
   resource_group_name = azurerm_resource_group.demo.name
-  location = azurerm_resource_group.demo.location
-  allocation_method = "Static"
+  location            = azurerm_resource_group.demo.location
+  allocation_method   = "Static"
   tags = {
     "terraform" = "terraform"
   }
@@ -67,10 +67,60 @@ resource "azurerm_network_interface" "mynic" {
   location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
   ip_configuration {
-    name = "internal"
-    subnet_id = azurerm_subnet.mysubnet.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.myip.id
+    public_ip_address_id          = azurerm_public_ip.myip.id
   }
 }
+resource "azurerm_mysql_server" "example" {
+  name                = "mysqlserver"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
 
+  administrator_login          = "adminuser"
+  administrator_login_password = "Arya@2000"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+resource "azurerm_virtual_machine" "myvm" {
+  name                  = "myvm"
+  location              = azurerm_resource_group.demo.location
+  resource_group_name   = azurerm_resource_group.demo.name
+  network_interface_ids = [azurerm_network_interface.mynic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
